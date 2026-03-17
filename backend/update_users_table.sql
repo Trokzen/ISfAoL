1051950aa8a3501becd513c178a7cb3b4a0bd0c6
Robert <Nadzar@yandex.ru>
2026-03-17 15:47:28 +0300

    Delete backend directory

--- backend/update_users_table.sql
+++
@@ -1,5 +0,0 @@
--- Добавляем поле fio в таблицу users для совместимости с моделью
-ALTER TABLE elibrary.users ADD COLUMN fio VARCHAR(255);
-
--- Обновляем существующие записи, чтобы fio совпадало с full_name
-UPDATE elibrary.users SET fio = full_name WHERE fio IS NULL;
